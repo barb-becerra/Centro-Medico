968b24b6cab2a58fb4c50d199c58b69c98bfca36
Barbara Becerra <barb.becerra@duocuc.cl>
2021-09-25 20:31:47 -0300

    login y signup

--- Centro-Medico-Galenos/src/app/pages/login/login.page.ts
+++ Centro-Medico-Galenos/src/app/pages/login/login.page.ts
@@ -1,3 +1,4 @@
+import { AlertController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import{
   FormGroup,
@@ -14,7 +15,7 @@ export class LoginPage implements OnInit {
 
   formLogin: FormGroup;
 
-  constructor(public fb: FormBuilder) { 
+  constructor(public fb: FormBuilder, public alertController: AlertController) { 
     this.formLogin = this.fb.group({
       'name': new FormControl("",Validators.required),
       'password': new FormControl("",Validators.required)
@@ -23,5 +24,21 @@ export class LoginPage implements OnInit {
 
   ngOnInit() {
   }
+  async ingresar(){
+    var f= this.formLogin.value;
+    var user =JSON.parse(localStorage.getItem('user'));
+    if(user.name == f.name && user.password ==f.password){
+      console.log('Ingresar');
 
-}
+    }else{
+      const alert = await this.alertController.create({
+        header: 'Datos incorrectos',
+        message: 'Usuario no registrado!,  Por favor registrate antes de intentar iniciar sesion.',
+        buttons: ['Entendido!']
+      });
+  
+      await alert.present();
+      return;
+    }
+    }
+  }
